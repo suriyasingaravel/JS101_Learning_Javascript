7a885e4dd840a54cda0ccbd8b32b7d757453f099
suriyasingaravel <suriyasingaravel710@gmail.com>
2023-04-18 21:49:53 +0530

    variables

---
+++ problem1.js
@@ -0,0 +1,10 @@
+console.log(10+17);
+console.log(10%4);
+console.log(15/3);
+console.log(15%3);
+console.log(56%125);
+console.log(125/34);
+console.log(1.099999%1);
+console.log(1/878.0999);
+
+console.log(1.099997890/1);
---
+++ problem2.js
@@ -0,0 +1,9 @@
+let a=8;
+
+let b=7;
+
+let c=4;
+console.log(a**c);
+
+console.log(c**5);
+console.log ((c**2)**(a**1));
